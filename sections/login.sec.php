f901444bc822c312b1215284922302ec4e895fcd
gpzhumphrey <gpzhumphrey@fa04502a-276b-11de-801e-eb64e24a040e>
2010-06-16 18:30:53 +0000

    One last bug fix.

--- sections/login.sec.php
+++ sections/login.sec.php
@@ -18,7 +18,9 @@
   	</tr>
 </table>
 </form>
-<?php if ($msg != "default") { ?><p><span class="icon"><img src="images/exclamation.png"   alt="Exclamation" /></span><span class="data">Have you <a href="index.php?section=register">registered your account</a> yet?</span></p><?php } ?>
+<?php if (lesserDate($today,$reg_deadline)) {
+ if ($msg != "default") { ?><p><span class="icon"><img src="images/exclamation.png"   alt="Exclamation" /></span><span class="data">Have you <a href="index.php?section=register">registered your account</a> yet?</span></p>
+<?php } } ?>
 <p><span class="icon"><img src="images/exclamation.png"   alt="Exclamation" /></span><span class="data">Did you forget your password? If so, <a href="index.php?section=login&amp;action=forgot">click here to reset it</a>.</span></p>
 <?php
 } 
