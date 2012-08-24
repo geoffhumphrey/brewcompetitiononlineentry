61657a4f3696183182375409cbb67477869ad930
alstoone@gmail.com <alstoone@gmail.com@fa04502a-276b-11de-801e-eb64e24a040e>
2012-08-24 19:38:36 +0000

    replaced call to eregi with case insensitive preg_match call as erergi is depreciated in php 5.3 and fails with error.

--- includes/email-ajax.inc.php
+++ includes/email-ajax.inc.php
@@ -4,7 +4,7 @@ $email=$_GET['email'];
 
 //echo $email;
 
-if (!eregi("^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,3})$", $email))
+if (!preg_match('/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,3})$/i', $email))
 
 	{
 
