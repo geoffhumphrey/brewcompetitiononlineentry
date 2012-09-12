c4b08322dd277d9ae66ff26ebaed067cfff02749
gpzhumphrey@gmail.com <gpzhumphrey@gmail.com@fa04502a-276b-11de-801e-eb64e24a040e>
2012-09-12 18:52:47 +0000

    Some cleanup.

--- trunk/admin/participants.admin.php
+++ trunk/admin/participants.admin.php
@@ -8,7 +8,7 @@
  *
  */
  
-function date_created($uid,$date_format,$time_format) {
+function date_created($uid,$date_format,$time_format,$timezone) {
 	require(CONFIG.'config.php');
 	
 	$query_user = sprintf("SELECT userCreated FROM %s WHERE id = '%s'", $prefix."users",$uid);
@@ -17,7 +17,7 @@ function date_created($uid,$date_format,$time_format) {
 	$totalRows_user = mysql_num_rows($user);
 	
 	if (($totalRows_user == 1) && ($row_user['userCreated'] != "")) {
-	$result = getTimeZoneDateTime($row_prefs['prefsTimeZone'], strtotime($row_user['userCreated']), $date_format,  $time_format, "short", "date-time-no-gmt");
+	$result = getTimeZoneDateTime($timezone, strtotime($row_user['userCreated']), $date_format,  $time_format, "short", "date-time-no-gmt");
 	}
 	
 	else $result = "&nbsp;";
@@ -75,7 +75,7 @@ if ($dbTable != "default") echo ": ".get_suffix($dbTable); ?></h2>
             </div>
   	</span>
   	<span class="adminSubNav">
-		<?php if ((totalRows_participant_count > $row_prefs['prefsRecordLimit']) && ($filter == "default")) { ?>
+		<?php if (($totalRows_participant_count > $row_prefs['prefsRecordLimit']) && ($filter == "default")) { ?>
   		<span class="icon"><img src="images/printer.png" /></span>
   			<div class="menuBar"><a class="menuButton" href="#" onclick="#" onmouseover="buttonMouseover(event, 'printMenu_participants_all');">Print <em>All</em></a></div>
   			<div id="printMenu_participants_all" class="menu" onmouseover="menuMouseover(event)">
@@ -319,7 +319,7 @@ if ($totalRows_participant_count > 0) {
     	<td class="dataList<?php if ($action == "print") echo " bdr1B"; ?>"><?php echo bjcp_rank($row_brewer['brewerJudgeRank'],1); if ($row_brewer['brewerJudgeMead'] == "Y") { echo "<br />"; if ($action != "print") echo "<span class='icon'><img src='images/star.png' alt='' title='Certified Mead Judge'></span>"; echo "Certified Mead Judge"; } ?></td>
 	  	<?php } 
 	}?>
-    <td class="dataList<?php if ($action == "print") echo " bdr1B"; ?>"><?php echo date_created($row_brewer['uid'],$row_prefs['prefsDateFormat'],  $row_prefs['prefsTimeFormat']); ?></td>
+    <td class="dataList<?php if ($action == "print") echo " bdr1B"; ?>"><?php echo date_created($row_brewer['uid'],$row_prefs['prefsDateFormat'],  $row_prefs['prefsTimeFormat'],$row_prefs['prefsTimeZone']); ?></td>
  <?php  if (($action != "print") && ($dbTable == "default")) { ?>
     <td class="dataList" nowrap="nowrap"><span class="icon"><a href="index.php?section=brew&amp;go=entries&amp;filter=<?php echo $row_user1['id']; ?>&amp;action=add&filter=<?php echo $row_brewer['uid']; ?>"><img src="images/book_add.png"  border="0" alt="Add an entry for <?php echo $row_brewer['brewerLastName'].", ".$row_brewer['brewerFirstName']; ?>" title="Add an entry for <?php echo $row_brewer['brewerLastName'].", ".$row_brewer['brewerFirstName']; ?>"></a></span><span class="icon"><a href="index.php?section=brewer&amp;go=admin&amp;filter=<?php echo $row_brewer['uid']; ?>&amp;action=edit&amp;id=<?php echo $row_brewer['id']; ?>"><img src="images/pencil.png"  border="0" alt="Edit <?php echo $row_brewer['brewerLastName'].", ".$row_brewer['brewerFirstName']; ?>" title="Edit <?php echo $row_brewer['brewerLastName'].", ".$row_brewer['brewerFirstName']; ?>"></a></span><span class="icon"><a href="index.php?section=admin&amp;go=make_admin&amp;username=<?php echo urlencode($row_brewer['brewerEmail']);?>"><img src="images/<?php if ($row_user1['userLevel'] == "1") echo "lock_open.png"; else echo "lock_edit.png"; ?>" border="0" alt="Change <?php echo $row_brewer['brewerLastName'].", ".$row_brewer['brewerFirstName']; ?>'s User Level" title="Change <?php echo $row_brewer['brewerLastName'].", ".$row_brewer['brewerFirstName']; ?>'s User Level"></a></span><?php if (($row_brewer['brewerAssignment'] == "J")) { ?><span class="icon"><a href="output/labels.php?section=admin&amp;go=participants&amp;action=judging_labels&amp;id=<?php echo $row_brewer['id']; ?>"><img src="images/page_white_acrobat.png"  border="0" alt="Download judging labels for <?php echo $row_brewer['brewerLastName'].", ".$row_brewer['brewerFirstName']; ?>" title="Download judging labels for <?php echo $row_brewer['brewerLastName'].", ".$row_brewer['brewerFirstName']; ?>"></a></span><?php } ?><span class="icon"><?php if ($row_brewer['brewerEmail'] == $_SESSION['loginUsername']) echo "&nbsp;"; else { ?><a href="javascript:DelWithCon('includes/process.inc.php?section=<?php echo $section; ?>&amp;go=<?php echo $go; ?>&amp;dbTable=<?php echo $brewer_db_table; ?>&amp;action=delete&amp;uid=<?php echo $row_brewer['uid'];?>','id',<?php echo $row_brewer['id']; ?>,'Are you sure you want to delete the participant <?php echo $row_brewer['brewerLastName'].", ".$row_brewer['brewerFirstName']; ?>?\nALL entries for this participant WILL BE DELETED as well.\nThis cannot be undone.');"><img src="images/bin_closed.png"  border="0" alt="Delete <?php echo $row_brewer['brewerLastName'].", ".$row_brewer['brewerFirstName']; ?>" title="Delete <?php echo $row_brewer['brewerLastName'].", ".$row_brewer['brewerFirstName']; ?>"></a><?php } ?></span>
     </td> 
