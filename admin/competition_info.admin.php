ac0bf922ff81d098b45fea9cd28b2f1047b4402d
geoffhumphrey <507357@den-e88543m.la.frd.directv.com>
2016-06-28 07:45:11 -0600

    Fix for Issue #632

--- admin/competition_info.admin.php
+++ admin/competition_info.admin.php
@@ -292,7 +292,7 @@
         <p>Each entry will consist of 12 to 22 ounce capped bottles or corked bottles that are void of all identifying information, including labels and embossing. Printed caps are allowed, but must be blacked out completely.</p>
 		<p>12oz brown glass bottles are preferred; however, green and clear glass will be accepted. Swing top bottles will likewise be accepted as well as corked bottles.</p>
 		<p>Bottles will not be returned to contest entrants.</p>
-		<p>Completed entry forms and recipe sheets must be submitted with all entries, and can be printed directly from this website. Entry forms should be attached to bottles with a rubber band only; glue and/or tape are unacceptable.</p>
+		<p>Completed entry forms and recipe sheets must be submitted with all entries, and can be printed directly from this website. Entry forms should be attached to bottle labels by the method specified on the bottle label.</p>
 		<p>Please fill out the entry forms completely. Be meticulous about noting any special ingredients that must be specified. Failure to note such ingredients may impact the judges' scoring of your entry.</p>
 		<p>Brewers are not limited to one entry in each category but may only enter each subcategory once.</p>
 		<?php } ?>
