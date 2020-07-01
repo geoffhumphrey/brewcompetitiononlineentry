geoffhumphrey/brewcompetitiononlineentry

commit 8d001dfe0e6333a723d87973b388f750c5bad578

Fix Setup Related Errors - Address Issue #1154

includes/constants.inc.php
@@ -2358,6 +2358,8 @@ $club_array = array(
 );
 asort($club_array);
 
+$sidebar_date_format = "short";
+
 if (((strpos($section, "step") === FALSE) && ($section != "setup")) && ($section != "update")) {
 
 	$registration_open = open_or_closed(time(),$row_contest_dates['contestRegistrationOpen'],$row_contest_dates['contestRegistrationDeadline']);
@@ -2375,7 +2377,6 @@ if (((strpos($section, "step") === FALSE) && ($section != "setup")) && ($section
 	*/
 
     if (strpos($_SESSION['prefsLanguage'],"en-") !== false) $sidebar_date_format = "long";
-    else $sidebar_date_format = "short";
 
 	$reg_open = getTimeZoneDateTime($_SESSION['prefsTimeZone'], $row_contest_dates['contestRegistrationOpen'], $_SESSION['prefsDateFormat'],  $_SESSION['prefsTimeFormat'], $sidebar_date_format, "date-time");
 	$reg_closed = getTimeZoneDateTime($_SESSION['prefsTimeZone'], $row_contest_dates['contestRegistrationDeadline'], $_SESSION['prefsDateFormat'],  $_SESSION['prefsTimeFormat'], $sidebar_date_format, "date-time");
@@ -2532,4 +2533,16 @@ else $optional_info_styles = array("21-B","28-A","30-B","33-A","33-B","34-B","M2
 $results_method = array("0" => "By Table", "1" => "By Style", "2" => "By Sub-Style");
 
 if (HOSTED) $_SESSION['prefsCAPTCHA'] = 1;
+
+// Load libraries only when needed - for performance
+$tinymce_load = array("contest_info","default","step4","default");
+$datetime_load = array("contest_info","eval","testing","preferences","step4","step5","step6","default","judging");
+$datatables_load = array("admin","list","default","step4","eval");
+
+// Default to English language if prefs not defined
+$prefsLanguage = "en-US";
+$prefsLanguageFolder = "en";
+if (isset($_SESSION['prefsLanguage'])) $prefsLanguage = $_SESSION['prefsLanguage'];
+if (isset($_SESSION['prefsLanguageFolder'])) $prefsLanguageFolder = $_SESSION['prefsLanguageFolder'];
+
 ?>

lang/language.lang.php
@@ -11,11 +11,12 @@ TODO - convert the following for translation:
   -- Be sure to also update function in common.lib.php
 */
 
+
 // Load public pages language file
-include (LANG.$_SESSION['prefsLanguageFolder'].DIRECTORY_SEPARATOR.$_SESSION['prefsLanguage'].'.lang.php');
+include (LANG.$prefsLanguageFolder.DIRECTORY_SEPARATOR.$prefsLanguage.'.lang.php');
 //include (LANG.'en'.DIRECTORY_SEPARATOR.'en-US.lang.php');
 
 // Load admin pages language file
 // A future version will have full conversions for Admin, Update, and Setup
-include (LANG.$_SESSION['prefsLanguageFolder'].DIRECTORY_SEPARATOR.$_SESSION['prefsLanguage'].'_admin.lang.php');
+include (LANG.$prefsLanguageFolder.DIRECTORY_SEPARATOR.$prefsLanguage.'_admin.lang.php');
 ?>

includes/db/common.db.php
@@ -278,8 +278,8 @@ if (isset($_SESSION['loginUsername'])) {
 if ((!isset($_SESSION['prefsLang'.$prefix_session]))|| (empty($_SESSION['prefsLang'.$prefix_session]))) {
 
 	// Language - in current version only English is available. Future versions will feature translations.
-	if ($section != "update") $_SESSION['prefsLanguage'] = $row_prefs['prefsLanguage'];
-	elseif (!isset($row_prefs['prefsLanguage'])) {
+	if (($section != "update") && (empty($row_prefs['prefsLanguage']))) $_SESSION['prefsLanguage'] = $row_prefs['prefsLanguage'];
+	elseif ((!isset($row_prefs['prefsLanguage'])) || (empty($row_prefs['prefsLanguage']))) {
 		$_SESSION['prefsLanguage'] = "en-US";
 	}
 	else $_SESSION['prefsLanguage'] = "en-US";

includes/load_cdn_libraries.inc.php
@@ -24,7 +24,7 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.13/jquery.mousewheel.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.pack.js"></script>
 
-    <?php if (($section == "admin") && (in_array($go,$datetime_load)) || ($section == "brew")) { ?>
+    <?php if ((($section == "admin") || (strpos($section, 'step') !== FALSE)) && (in_array($go,$datetime_load)) || ($section == "brew")) { ?>
     <!-- Load Bootstrap DateTime Picker / http://eonasdan.github.io/bootstrap-datetimepicker/ -->
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/css/bootstrap-datetimepicker.min.css" />
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.1/moment-with-locales.min.js"></script>
@@ -32,7 +32,7 @@
     <script src="<?php echo $base_url;?>js_includes/date-time.min.js"></script>
     <?php } ?>
 
-	<?php if (($section == "admin") && (in_array($go,$tinymce_load))) { ?>
+	<?php if ((($section == "admin") || (strpos($section, 'step') !== FALSE)) && (in_array($go,$tinymce_load))) { ?>
     <!-- Load TinyMCE / https://www.tinymce.com/ -->
     <?php if (ENABLE_MARKDOWN) { ?>
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-markdown-editor/2.0.2/css/bootstrap-markdown-editor.css">
@@ -50,7 +50,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jasny-bootstrap/3.1.3/css/jasny-bootstrap.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jasny-bootstrap/3.1.3/js/jasny-bootstrap.min.js"></script>
 
-	<?php if (($section == "admin") && (($go == "upload") || ($go == "upload_scoresheets"))) { ?>
+	<?php if ((($section == "admin") || (strpos($section, 'step') !== FALSE)) && (($go == "upload") || ($go == "upload_scoresheets"))) { ?>
     <!-- Load DropZone / http://www.dropzonejs.com -->
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.2.0/min/dropzone.min.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.2.0/min/dropzone.min.js"></script>

includes/load_local_libraries.inc.php
@@ -25,7 +25,7 @@
     <script type="text/javascript" src="<?php echo $base_url; ?>libraries/fancybox/lib/jquery.mousewheel-3.0.6.pack.js"></script>
     <script type="text/javascript" src="<?php echo $base_url; ?>libraries/fancybox/source/jquery.fancybox.pack.js"></script>
     
-    <?php if (($section == "admin") && (in_array($go,$datetime_load)) || ($section == "brew")) { ?>
+    <?php if ((($section == "admin") || (strpos($section, 'step') !== FALSE)) && (in_array($go,$datetime_load)) || ($section == "brew")) { ?>
     
     <!-- Load Bootstrap DateTime Picker / http://eonasdan.github.io/bootstrap-datetimepicker/ -->
     <link rel="stylesheet" type="text/css" href="<?php echo $base_url; ?>libraries/date-time-picker/css/bootstrap-datetimepicker.min.css" />
@@ -34,7 +34,7 @@
     <script src="<?php echo $base_url;?>js_includes/date-time.min.js"></script>
     <?php } ?>
     
-    <?php if (($section == "admin") && (in_array($go,$tinymce_load))) { ?>
+    <?php if ((($section == "admin") || (strpos($section, 'step') !== FALSE)) && (in_array($go,$tinymce_load))) { ?>
     <!-- Load TinyMCE / https://www.tinymce.com/ -->
     <script src="<?php echo $base_url; ?>libraries/tinymce/js/tinymce/tinymce.min.js"></script>
 	<script src="<?php echo $base_url;?>js_includes/tinymce-init.min.js"></script>    
@@ -46,7 +46,7 @@
     <link rel="stylesheet" href="<?php echo $base_url; ?>libraries/jasny-bootstrap/css/jasny-bootstrap.min.css">
     <script src="<?php echo $base_url; ?>libraries/jasny-bootstrap/js/jasny-bootstrap.min.js"></script>
     
-	<?php if (($section == "admin") && (($go == "upload") || ($go == "upload_scoresheets"))) { ?>
+	<?php if ((($section == "admin") || (strpos($section, 'step') !== FALSE)) && (($go == "upload") || ($go == "upload_scoresheets"))) { ?>
     <!-- Load DropZone / http://www.dropzonejs.com -->
     <link rel="stylesheet" type="text/css" href="<?php echo $base_url; ?>libraries/dropzone/dropzone.min.css" />
     <script src="<?php echo $base_url; ?>libraries/dropzone/dropzone.min.js"></script>

includes/logincheck.inc.php
@@ -155,7 +155,6 @@ if ($check == 1) {
 	//echo $_SESSION['loginUsername']."<br>";
 	//echo $location."<br>";
 	//echo "Yes."."<br>";
-	
 	//exit;
 	
 }

index.php
@@ -75,10 +75,10 @@ else {
     $nav_container = "navbar-default";
 }
 
-// Load libraries only when needed - for performance
-$tinymce_load = array("contest_info","default","step4","default");
-$datetime_load = array("contest_info","eval","testing","preferences","step4","step5","step6","default","judging");
-$datatables_load = array("admin","list","default","step4","eval");
+/* 
+ * Load libraries only when needed - for performance
+ * Moved to constants.inc.php - 2.1.19
+ */
 
 $security_question = array($label_secret_01, $label_secret_05, $label_secret_06, $label_secret_07, $label_secret_08, $label_secret_09, $label_secret_10, $label_secret_11, $label_secret_12, $label_secret_13, $label_secret_14, $label_secret_15, $label_secret_16, $label_secret_17, $label_secret_18, $label_secret_19, $label_secret_20, $label_secret_21, $label_secret_22, $label_secret_23, $label_secret_25, $label_secret_26, $label_secret_27);
 

paths.php
@@ -159,7 +159,7 @@ else ini_set('display_errors','Off');
  */
 
 function is_https() {
-    if (((!empty($_SERVER['HTTPS'])) && (strtolower($_SERVER['HTTPS']) !== "off")) || ((isset($_SERVER['SERVER_PORT'])) && ($_SERVER['SERVER_PORT'] === "443"))) return true;
+    if (((!empty($_SERVER['HTTPS'])) && (strtolower($_SERVER['HTTPS']) !== "off")) || ((isset($_SERVER['SERVER_PORT'])) && ($_SERVER['SERVER_PORT'] === "443"))) return TRUE;
     elseif (((!empty($_SERVER['HTTP_X_FORWARDED_PROTO'])) && (strtolower($_SERVER['HTTP_X_FORWARDED_PROTO']) == "https")) || ((!empty($_SERVER['HTTP_X_FORWARDED_SSL'])) && (strtolower($_SERVER['HTTP_X_FORWARDED_SSL']) == "on"))) return TRUE;
     else return FALSE;
 }

commit b5558736a6d8d34b92d544dbae9bf273ba3c697f

Correct Language Bug Introduced in Commit 8d001df

includes/constants.inc.php
@@ -2539,10 +2539,4 @@ $tinymce_load = array("contest_info","default","step4","default");
 $datetime_load = array("contest_info","eval","testing","preferences","step4","step5","step6","default","judging");
 $datatables_load = array("admin","list","default","step4","eval");
 
-// Default to English language if prefs not defined
-$prefsLanguage = "en-US";
-$prefsLanguageFolder = "en";
-if (isset($_SESSION['prefsLanguage'])) $prefsLanguage = $_SESSION['prefsLanguage'];
-if (isset($_SESSION['prefsLanguageFolder'])) $prefsLanguageFolder = $_SESSION['prefsLanguageFolder'];
-
 ?>

lang/language.lang.php
@@ -11,10 +11,14 @@ TODO - convert the following for translation:
   -- Be sure to also update function in common.lib.php
 */
 
+// Default to English language if prefs not defined
+$prefsLanguage = "en-US";
+$prefsLanguageFolder = "en";
+if (isset($_SESSION['prefsLanguage'])) $prefsLanguage = $_SESSION['prefsLanguage'];
+if (isset($_SESSION['prefsLanguageFolder'])) $prefsLanguageFolder = $_SESSION['prefsLanguageFolder'];
 
 // Load public pages language file
 include (LANG.$prefsLanguageFolder.DIRECTORY_SEPARATOR.$prefsLanguage.'.lang.php');
-//include (LANG.'en'.DIRECTORY_SEPARATOR.'en-US.lang.php');
 
 // Load admin pages language file
 // A future version will have full conversions for Admin, Update, and Setup