347b7bb56c6d51643260796c1b52c3dfc06a892a
geoffhumphrey <ghumphrey1971@gmail.com>
2021-12-08 15:10:56 -0700

    Update current_version.inc.php

--- includes/current_version.inc.php
+++ includes/current_version.inc.php
@@ -1,6 +1,6 @@
 <?php
 // For storage in DB and update script use
-$current_version = "2.3.2.0";
+$current_version = "2.3.2.1";
 
 // For on-screen display and consistency
 $current_version_display = "2.3.2";
@@ -10,7 +10,7 @@ $current_version_display_append = "";
 if (!empty($current_version_display_append)) $current_version_display .= " ".$current_version_display_append;
 
 // Change date for each pre-release and release. Will trigger a force update.
-$current_version_date_display = "2021-11-30";
+$current_version_date_display = "2021-12-08";
 
 // Convert current version date to Unix timestamp
 $current_version_date = strtotime($current_version_date_display);
