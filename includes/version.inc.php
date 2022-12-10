93614fe8fcd03cdd3647bb6b489ffa5a5445f4b2
Gene Wood <gene_wood@cementhorizon.com>
2022-12-10 10:07:50 -0800

    Update comment pointing to table containing version

--- includes/version.inc.php
+++ includes/version.inc.php
@@ -6,6 +6,6 @@ Checked Single
 */
 
 // This file will be deprecated after the release of version 1.2.1.0
-// Version information will be stored in the system table starting with version 1.2.1.0
+// Version information will be stored in the bcoem_sys table starting with version 1.2.1.0
 $version = "1.1.4.0";
 ?>
