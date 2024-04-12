247804c5084cb1893ae457544672a3b647633f66
geoffhumphrey <geoffhumphrey@users.noreply.github.com>
2024-04-12 11:17:35 -0600

    Final 2.7.0.0
    Fix Info Icon bug in winners display.

--- sections/winners.sec.php
+++ sections/winners.sec.php
@@ -148,9 +148,11 @@ if ($row_scored_entries['count'] > 0) {
 						$winners_table_body_1 .= "<td width=\"25%\">";
 						if ($_SESSION['prefsStyleSet'] == "BA") $winners_table_body_1 .= $row_scores['brewStyle'];
 						else $winners_table_body_1 .= $style.": ".$row_scores['brewStyle'];
+
 						if ((!empty($row_scores['brewInfo'])) && ($section != "results")) {
-							$winners_table_body_1 .= " <a href=\"#".$row_scores['id']."\"  tabindex=\"0\" role=\"button\" data-toggle=\"popover\" data-trigger=\"hover\" data-placement=\"auto top\" data-container=\"body\" title=\"".$label_info."\" data-content=\"".str_replace("^", " ", $row_scores['brewInfo'])."\"><span class=\"hidden-xs hidden-sm hidden-md hidden-print fa fa-info-circle\"></span></a></td>";
+							$winners_table_body_1 .= " <a href=\"#".$row_scores['id']."\" tabindex=\"0\" role=\"button\" data-toggle=\"popover\" data-trigger=\"hover\" data-placement=\"auto top\" data-container=\"body\" title=\"".$label_info."\" data-content=\"".str_replace("^", " ", $row_scores['brewInfo'])."\"><span class=\"hidden-xs hidden-sm hidden-md hidden-print fa fa-info-circle\"></span></a>";
 						}
+
 						$winners_table_body_1 .= "</td>";
 
 						if ($_SESSION['prefsProEdition'] == 0) {
