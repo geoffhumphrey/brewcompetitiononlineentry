601f1217504d944ae3791438cef0f94459d73647
geoffhumphrey <geoffhumphrey@users.noreply.github.com>
2026-01-30 14:24:06 -0700

    Update index.php

--- index.php
+++ index.php
@@ -14,9 +14,9 @@ require_once (CONFIG.'bootstrap.php');
 if (!HOSTED) require_once (DB.'mods.db.php');
 
 header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
-//header('Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . ' GMT');
+header('Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . ' GMT');
 header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
-//header('Cache-Control: post-check=0, pre-check=0', false);
+header('Cache-Control: post-check=0, pre-check=0', false);
 header('Pragma: no-cache');
 
 // Good for 3.0.0+
