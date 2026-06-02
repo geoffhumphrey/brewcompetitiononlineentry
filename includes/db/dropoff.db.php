28298ac0136c0b9d31114c747a3883e6d4db8c43
geoffhumphrey <geoffhumphrey@users.noreply.github.com>
2026-06-02 16:31:15 -0600

    Fix Bug when Editing Pro Participants
    No drop off locations were available as a choice.

--- includes/db/dropoff.db.php
+++ includes/db/dropoff.db.php
@@ -1,15 +1,8 @@
 <?php
 $query_dropoff = sprintf("SELECT * FROM %s",$drop_off_db_table);
-if (SINGLE) {
-	$query_dropoff .= sprintf(" WHERE comp_id='%s'",$_SESSION['comp_id']);
-	if (($section == "admin") && ($action == "edit")) $query_dropoff .= sprintf(" AND id='%s'",$id);
-    else $query_dropoff .= " ORDER BY dropLocationName ASC";
-}
-else {
-	if (($section == "admin") && ($action == "edit")) $query_dropoff .= sprintf(" WHERE id='%s'",$id);
-    else $query_dropoff .= " ORDER BY dropLocationName ASC";
-}
-$dropoff = mysqli_query($connection,$query_dropoff) or die (mysqli_error($connection));
+if (($section == "admin") && ($action == "edit") && ($go == "dropoff")) $query_dropoff .= sprintf(" WHERE id='%s'",$id);
+else $query_dropoff .= " ORDER BY dropLocationName ASC";
+$dropoff = mysqli_query($connection,$query_dropoff) or die ("A database error occurred.");
 $row_dropoff = mysqli_fetch_assoc($dropoff);
 $totalRows_dropoff = mysqli_num_rows($dropoff);
 ?>
