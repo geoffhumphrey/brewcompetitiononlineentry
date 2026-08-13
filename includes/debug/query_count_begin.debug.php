aaa7ecb577e636e6480ad41c6276946ccd0b540f
delight-f <76928118+delight-f@users.noreply.github.com>
2026-08-14 06:54:09 +1000

    refactor: add strict_types to includes/convert and includes/debug
    - declare(strict_types=1) in 4 style-conversion scripts + 3 debug helpers
    - verified: php -l clean

--- includes/debug/query_count_begin.debug.php
+++ includes/debug/query_count_begin.debug.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 $output_query_count = "";
 if (isset($_SESSION['userLevel'])) {
 	if ($_SESSION['userLevel'] == 0) {
--- includes/debug/session_vars.debug.php
+++ includes/debug/session_vars.debug.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 // ---------------- DEBUG SESSION VARS --------------------
 // echo phpinfo();
 $table_body = "";
