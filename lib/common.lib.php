59d457764612fad49c6d6b20b9f5c6a0b14a0eaf
Scott Ulrich <152253240+auspisec@users.noreply.github.com>
2026-08-16 09:43:21 +0900

    Add Korean Won (KRW) currency support (#1710)
    Co-authored-by: Synthia <synthia@hermes-agent>

--- lib/common.lib.php
+++ lib/common.lib.php
@@ -649,6 +649,7 @@ function currency_info($input,$method) {
 			case "tlira": $currency_code = "&#8356;^TRY"; break;
 			case "R": $currency_code = "R^ZAR"; break;
 			case "rupee": $currency_code = "&#8360;^INR"; break;
+			case "krw": $currency_code = "&#8361;^KRW"; break;
 		}
 
 	}
@@ -683,7 +684,8 @@ function currency_info($input,$method) {
 			"tlira^&#8356; Turkish Lira^TRY",
 			" ^---------------------^-----------",
 			"R^R South African Rand^ZAR",
-			"rupee^&#8360; Rupee^INR"
+			"rupee^&#8360; Rupee^INR",
+			"krw^&#8361; Won^KRW"
 		);
 
 	}
